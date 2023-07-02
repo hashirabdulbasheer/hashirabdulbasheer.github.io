814d5e6219dba99067caae2eb871b4c1b12ea8d0
hashirabdulbasheer <hashir.abdulbasheer@gmail.com>
2023-07-02 05:06:50 +0000

    update: Quran Ayat app

--- apps/quran-ayat/flutter_service_worker.js
+++ apps/quran-ayat/flutter_service_worker.js
@@ -152,7 +152,7 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "index.html": "2fc908ae86fd271af407ad808c70dae1",
 "/": "2fc908ae86fd271af407ad808c70dae1",
-"main.dart.js": "d59406c2a1bd45c6decf979b619c3a35",
+"main.dart.js": "ff59e7e300f5748b581b08ddd139aea2",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
